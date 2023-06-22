14f5ef7c362d043ead6b1b91b7aff49dae1fa128
Student User <student@workstation.lab.example.com>
2023-06-22 02:26:05 -0400

    Add release stage

--- shipping-calculator/src/test/java/com/redhat/shipping/ShippingCalculatorTest.java
+++ shipping-calculator/src/test/java/com/redhat/shipping/ShippingCalculatorTest.java
@@ -6,5 +6,34 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ShippingCalculatorTest {
 
-    // @todo: add tests
+	 
+      @Test
+
+      public void canCalculateTheCostForARegion() {
+
+	     ShippingCalculator calculator = new ShippingCalculator();
+
+	      assertEquals(0, calculator.costForRegion("A Region"));
+
+    }
+    
+    @Test
+
+    public void onNARegionTheCostIs100() {
+
+	     // Given a shipping calculator
+	     //
+	     ShippingCalculator calculator = new ShippingCalculator();
+	     //
+	     //   // When NA is he country region
+	     //
+	     int calculatedCost = calculator.costForRegion("NA");
+	     assertEquals(100, calculatedCost);
+
+
+
+
+    }
 }
+
+
